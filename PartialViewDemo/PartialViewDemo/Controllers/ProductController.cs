3718586058f8a9a12cf0780322c5f78ee6da5498
Shoaib.Ghachi <shoaib.ghachi@hotmail.com>
2022-01-26 11:47:50 +0530

    pushing new project

---
+++ PartialViewDemo/PartialViewDemo/Controllers/ProductController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using PartialViewDemo.Models;
+namespace PartialViewDemo.Controllers
+{
+    public class ProductController : Controller
+    {
+        // GET: Product
+        public ActionResult Index()
+        {
+            List<Product> productList = new List<Product>
+            {
+                new Product{ProductID=1,ProductName="Keyboard",ProductPhoto="~/Products/Keyboard.jpeg"},
+                new Product{ProductID=2,ProductName="Monitor",ProductPhoto="~/Products/monitor.jpg"},
+                new Product{ProductID=3,ProductName="Mouse",ProductPhoto="~/Products/mouse.jpeg"}
+            };
+
+            return View(productList);
+        }
+    }
+}
---
+++ PartialViewDemo/PartialViewDemo/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace PartialViewDemo.Models
+{
+    public class Product
+    {
+        public int ProductID { get; set; }
+        public string ProductName { get; set; }
+        public string ProductPhoto { get; set; }
+    }
+
+}
